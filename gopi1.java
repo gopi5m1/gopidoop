2b78aed7d32b97a6a11050271187f6476a71ec35
gopi5m1 <41737824+gopi5m1@users.noreply.github.com>
2018-07-27 11:03:43 +0530

    Create gopi1.java

---
+++ gopi1.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+              int n=12;
+          if(n>0)
+         {
+         System.out.println("Positive");
+         }
+        else if(n<0)
+        {
+        System.out.println("Negative");
+        }
+        else
+        {
+        System.out.println("zero");
+        }
+}           
+}	
